60c1fc0abd3ec645a48f633836d2f11498f6cb9a
Matheus Teixeira <mteixeira@topconsuite.com>
2024-01-06 10:51:24 -0300

    feat(frontend): add api call and display on screen

--- Frontend/src/pages/index.tsx
+++ Frontend/src/pages/index.tsx
@@ -2,21 +2,29 @@ import { useState, useEffect } from "react";
 import { GiftCard, DetailsModal } from "@/molecules";
 import Styled from "@/styles/pages";
 import Modal from "react-modal";
-import { Gift } from "@/types";
+import { Gift } from "@shared/entities/gift.entity";
+import { StandartResponse } from "@shared/helpers/StandartResponse";
 import { cinzel } from "@/app/fonts";
-
-const gift: Gift = {
-  name: "Amazon Alexa",
-  description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam euismod, nisl eget aliquam ultricies, nunc nisl ultricies nunc, quis ultricies nisl nisl nec nisl.",
-  image_src: "/imgs/products/alexa.jpg",
-  price: 10000,
-  link: "https://www.amazon.com.br/Amazon-Echo-Dot-4%C2%AA-Gera%C3%A7%C3%A3o/dp/B084DWH3YF/ref=sr_1_1?dchild=1&keywords=alexa&qid=1632086597&sr=8-1"
-}
+import { api } from "@/services/api";
 
 export default function Home() {
+  const [giftsList, setGiftsList] = useState<Gift[]>([]);
   const [isOpen, setIsOpen] = useState(false)
 
-  useEffect(() => {console.log(isOpen)}, [isOpen]);
+  const getAllGifts = async () => {
+    try {
+      const response = await api.get<StandartResponse<Gift>>('/gifts');
+      console.log(response);
+      
+      setGiftsList(response.data.result);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  
+  useEffect(() => {
+    getAllGifts();
+  }, []);
 
   return (
    <Styled.HomeContainer>
@@ -28,83 +36,18 @@ export default function Home() {
         </Styled.TitleCard>
       </Styled.Cover>
       <Styled.Content>
-        <GiftCard
-          name="Amazon Alexa"
-          description="Smart Speaker"
-          imageSrc="/imgs/products/alexa.jpg"
-          value={10000}
-          onClick={() => setIsOpen(true)}
-        />
-        <GiftCard
-          name="Amazon Alexa"
-          description="Smart Speaker"
-          imageSrc="/imgs/products/alexa.jpg"
-          value={10000}
-          onClick={() => setIsOpen(true)}
-        />
-        <GiftCard
-          name="Amazon Alexa"
-          description="Smart Speaker"
-          imageSrc="/imgs/products/alexa.jpg"
-          value={10000}
-          onClick={() => setIsOpen(true)}
-        />
-        <GiftCard
-          name="Amazon Alexa"
-          description="Smart Speaker"
-          imageSrc="/imgs/products/alexa.jpg"
-          value={10000}
-          onClick={() => setIsOpen(true)}
-        />
-        <GiftCard
-          name="Amazon Alexa"
-          description="Smart Speaker"
-          imageSrc="/imgs/products/alexa.jpg"
-          value={10000}
-          onClick={() => setIsOpen(true)}
-        />
-        <GiftCard
-          name="Amazon Alexa"
-          description="Smart Speaker"
-          imageSrc="/imgs/products/alexa.jpg"
-          value={10000}
-          onClick={() => setIsOpen(true)}
-        />
-        <GiftCard
-          name="Amazon Alexa"
-          description="Smart Speaker"
-          imageSrc="/imgs/products/alexa.jpg"
-          value={10000}
-          onClick={() => setIsOpen(true)}
-        />
-        <GiftCard
-          name="Amazon Alexa"
-          description="Smart Speaker"
-          imageSrc="/imgs/products/alexa.jpg"
-          value={10000}
-          onClick={() => setIsOpen(true)}
-        />
-        <GiftCard
-          name="Amazon Alexa"
-          description="Smart Speaker"
-          imageSrc="/imgs/products/alexa.jpg"
-          value={10000}
-          onClick={() => setIsOpen(true)}
-        />
-        <GiftCard
-          name="Amazon Alexa"
-          description="Smart Speaker"
-          imageSrc="/imgs/products/alexa.jpg"
-          value={10000}
-          onClick={() => setIsOpen(true)}
-        />
-        <GiftCard
-          name="Amazon Alexa"
-          description="Smart Speaker"
-          imageSrc="/imgs/products/alexa.jpg"
-          value={10000}
-          onClick={() => setIsOpen(true)}
-        />
+        {
+          giftsList.map((gift) => (
+            <GiftCard
+              key={gift.gift_id}
+              name={gift.name}
+              description={gift.description}
+              imageSrc={gift.image_src}
+              value={gift.price}
+              onClick={() => setIsOpen(true)}
+            />
+          ))
+        }
       </Styled.Content>
    </Styled.HomeContainer>
   )

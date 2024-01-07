d589662aa15f731387943535a0658cab0923ea22
Matheus Teixeira <mteixeira@topconsuite.com>
2024-01-07 12:20:52 -0300

    fix(backend): remove bug in migration of stripe_id

--- Backend/src/database/migrations/1704549426833-AddReferenceNKitColumn_reg-gifts.ts
+++ Backend/src/database/migrations/1704549426833-AddReferenceNKitColumn_reg-gifts.ts
@@ -4,7 +4,6 @@ export class AddReferenceNKitColumnRegGifts1704549426833 implements MigrationInt
     name = 'AddReferenceNKitColumnRegGifts1704549426833'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE ${process.env.DB_SCHEMA}."reg_gifts" DROP COLUMN "stripe_id"`);
         await queryRunner.query(`ALTER TABLE ${process.env.DB_SCHEMA}."reg_gifts" ADD "kit_name" character varying(255)`);
         await queryRunner.query(`ALTER TABLE ${process.env.DB_SCHEMA}."reg_gifts" ADD "reference_url" character varying(255)`);
     }
@@ -12,7 +11,6 @@ export class AddReferenceNKitColumnRegGifts1704549426833 implements MigrationInt
     public async down(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`ALTER TABLE ${process.env.DB_SCHEMA}."reg_gifts" DROP COLUMN "reference_url"`);
         await queryRunner.query(`ALTER TABLE ${process.env.DB_SCHEMA}."reg_gifts" DROP COLUMN "kit_name"`);
-        await queryRunner.query(`ALTER TABLE ${process.env.DB_SCHEMA}."reg_gifts" ADD "stripe_id" character varying(255) NOT NULL`);
     }
 
 }

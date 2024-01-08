5598dcc84ab43bfba62089f735334b78986cc96b
Matheus Teixeira <mteixeira@topconsuite.com>
2024-01-07 21:04:03 -0300

    fix(frontend): fix env using next public

--- Frontend/src/services/api.ts
+++ Frontend/src/services/api.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: process.env.ENVIRONMENT === "PROD" ? "http://listacasanova.matheustsdev.com.br:5000" : "http://localhost:5000",
+    baseURL: process.env.NEXT_PUBLIC_ENVIRONMENT === "PROD" ? "http://listacasanova.matheustsdev.com.br:5000" : "http://localhost:5000",
 });
